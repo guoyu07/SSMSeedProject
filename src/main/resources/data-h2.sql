02b3fdc5d7465f3ba472e1236c64c81ffbe6e490
张志豪 <izhangzhihao@hotmail.com>
2017-05-29 15:06:35 +0800

    fix mapper

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,5 @@
 INSERT INTO USER (userName, passWord) VALUES ('admin', '9cf3e758a497c6274bd066d0b2168432f8a34aad95f63a65677a9a56acec94a7');
-INSERT INTO USER (userName, passWord,accountExpired,locked,credentialsExpired) VALUES ('testUser', 'test',0,0,1);
+INSERT INTO USER (userName, passWord,accountExpired,locked,credentialsExpired) VALUES ('testUser', 'test',1,1,0);
 
 INSERT INTO ROLE(roleName) VALUES ('ROLE_ADMIN');
 INSERT INTO ROLE(roleName) VALUES ('ROLE_USER');
