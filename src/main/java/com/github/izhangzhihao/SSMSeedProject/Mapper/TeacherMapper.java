57afc88b30534c19c66dde22b58799b704226ef5
张志豪 <izhangzhihao@hotmail.com>
2017-06-03 21:25:38 +0800

    cleanup code

--- src/main/java/com/github/izhangzhihao/SSMSeedProject/Mapper/TeacherMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.izhangzhihao.SSMSeedProject.Mapper;
-
-import com.github.izhangzhihao.SSMSeedProject.Model.Teacher;
-
-import java.util.List;
-
-public interface TeacherMapper {
-    List<Teacher> selectTeacherByStudentId(int id);
-}
